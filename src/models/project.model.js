8c37c814679dac1b444e671372e0ac5125808d67
devyapp <devyan2022@gmail.com>
2022-06-06 07:40:41 -0300

    second commit

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const projectModel = new Schema({
   title: { type: String, maxlength: 50, unique: true },
-  description: { type: String, maxlength: 500 },
+  content: { type: String },
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
